3d119ed2ce3351a09eb54cdca680fe5b0af4b1c1
hmm1031 <92812256+hmm1031@users.noreply.github.com>
2021-11-22 11:45:04 -0500

    Add files via upload

---
+++ Final Project R script.R
@@ -0,0 +1,34 @@
+install.packages("maptools")
+install.packages("rgdal")
+install.packages("raster")
+install.packages("rgdal")
+install.packages("maps")
+install.packages("mapdata")
+install.packages("ggmap")
+install.packages("Lattice")
+install.packages("sp")
+library(sp)
+library(maptools)
+library(rgdal)
+library(maptools)
+library(rgdal)
+library(raster)
+library(maps)
+library(mapdata)
+library(ggmap)
+library(marmap)
+library(lattice)
+library(ggplot2)
+
+register_google(key= "AIzaSyDmnvI2H2L3aVAOk4cdSaDLbYZPNn1DXyQ")
+
+myLocation <- c(lon = -70.7833, lat = 43.0989)
+my_points <- data.frame(lon = -70.7833, lat = 43.0989) #need to pull in CSV file earlier. Should be read in as a dataframe . Color=type
+
+
+myMap <- get_googlemap(center= myLocation, maptype= "satellite", zoom=11) 
+
+ggmap(myMap) + geom_point(data=my_points, aes(x=lon, y=lat), color="orange")
+
+
+
