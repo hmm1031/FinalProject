024154c6e33f253f4043056a61a603754448b553
chloe-pearson <92814376+chloe-pearson@users.noreply.github.com>
2021-11-30 18:42:40 -0500

    Add files via upload

---
+++ animations.R
@@ -0,0 +1,70 @@
+install.packages("maptools")
+install.packages("rgdal")
+install.packages("raster")
+install.packages("rgdal")
+install.packages("maps")
+install.packages("mapdata")
+install.packages("ggmap")
+install.packages("Lattice")
+install.packages("sp")
+install.packages('gifski')
+install.packages('png')
+library(sp)
+library(maptools)
+library(rgdal)
+library(maptools)
+library(rgdal)
+library(raster)
+library(maps)
+library(mapdata)
+library(ggmap)
+library(marmap)
+library(lattice)
+library(ggplot2)
+library(tidyverse)
+library(gganimate)
+library(gifski)
+library(png)
+
+register_google(key= "AIzaSyDmnvI2H2L3aVAOk4cdSaDLbYZPNn1DXyQ")
+
+myLocation <- c(lon = -70.7833, lat = 43.0989)
+
+myMap <- get_googlemap(center= myLocation, maptype= "satellite", zoom=11)
+
+ggmap(myMap)
+
+lat_long <- read.csv("receiver_lat_long.csv")
+
+all_detections <- read.csv("detections_animations.csv")
+
+detections <- left_join(all_detections, lat_long)
+
+#fish1 is 53786
+fish1 <- subset(detections, tag %in% c("53786"))
+#%R is equal to hour:minute
+fish1$date <- as.POSIXct(fish1$date, format = "%m/%d/%Y %R")
+
+anim <- ggmap(myMap) + 
+  geom_point(data=fish1, aes(x=longitude, y=latitude, group = date))+
+  transition_time(date)
+
+anim <- ggmap(myMap) + 
+  geom_sf(data=fish1, aes(x=longitude, y=latitude, group = date))+
+  transition_time(date)
+  
+gga <- ggplot(data = fish1, aes(x = longitude, y = latitude)) +
+  geom_point(size = 3, alpha = 0.5) +
+  transition_time(date) + 
+  labs(title = paste("Day", "{round(frame_time, 0)}")) +
+  shadow_wake(wake_length = 0.05)
+
+animate(gga)
+animate(gga, "output.gif")
+
+
+
+
+  
+
+
