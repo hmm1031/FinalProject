017c2cb23bd2e70eb9e6b82b9abf1ef61ba06e7a
Hanna Mogensen <hmm1031@wildcats.unh.edu>
2021-11-30 16:09:04 -0500

    obj 2 updated

--- Obj1_R script.R
+++ Obj1_R script.R
@@ -20,19 +20,23 @@ library(marmap)
 library(lattice)
 library(ggplot2)
 
-register_google(key= "AIzaSyDmnvI2H2L3aVAOk4cdSaDLbYZPNn1DXyQ")
+install.packages('usmap', dependencies= TRUE)
+library(usmap)
 
+register_google(key= "AIzaSyDmnvI2H2L3aVAOk4cdSaDLbYZPNn1DXyQ")
+#Map for Obj 1
 myLocation <- c(lon = -70.7833, lat = 43.0989)
 my_points <- data.frame(lon = -70.7833, lat = 43.0989) #need to pull in CSV file earlier. Should be read in as a dataframe . Color=type
-
-
 myMap <- get_googlemap(center= myLocation, maptype= "satellite", zoom=11) 
 
+
+
+#######
+
+#Obj 1: Map of all locations, with specific marker colors, shapes, and sizes
 lat_long <- read.csv("lat_longs_rfw.csv", stringsAsFactors = TRUE)
 
 AllPoints <- data.frame(lat_long)
-
-#Obj 1: Map of all locations, with specific marker colors, shapes, and sizes
 
 ggmap(myMap) + geom_point(data=AllPoints, aes(x=longitude, y=latitude, shape = type, color= type, size = type)) + scale_shape_manual (values= c(15,17,19)) + scale_size_manual(values = c(4,2,2)) + scale_color_manual (values=c("tomato1", "deeppink3", "turquoise")) + ggtitle("Map of Sampling Locations in Great Bay") + theme(plot.title = element_text(face = "bold")) + theme (plot.title = element_text(hjust = 0.5)) 
 
@@ -52,11 +56,19 @@ LatLong <- data.frame(LatLong)
 ReceiverPingLL <- left_join(ReceiverPings, LatLong) #insert y based on X
 View(ReceiverPingLL)
 #create summary table with counts for receiver number 
-ReceiverSummary <-ReceiverPingLL%>%
+ReceiverSummary <- ReceiverPingLL%>%
   select(receiver, latitude, longitude)%>%
-  count(ReceiverSummary, vars= "receiver", wt = NULL, sort = FALSE, name = "Frequency")
-#view new data set
+  dplyr::count(vars= receiver, latitude, longitude, name = "Frequency")
 View(ReceiverSummary)
+#make map!
+
+#install color package
+install.packages("viridis")
+library(viridis)
 
 
+ggmap(myMap) + geom_point(data=ReceiverSummary, aes(x=longitude, y=latitude, size=Frequency, color=Frequency)) +   scale_size_continuous(range=c(1,12)) +
+  scale_color_viridis(trans="log") +
+  theme_void() 
 
+##Reverse color pallet 
